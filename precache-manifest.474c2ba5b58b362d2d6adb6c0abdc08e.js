24ce3c986aef2d23daf1ce08fc0fbcbe813d411b
ma1ex <matvienko.alexey@gmail.com>
2019-07-09 16:12:01 +0300

    Updates

---
+++ precache-manifest.474c2ba5b58b362d2d6adb6c0abdc08e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6a8f1d0a9734472bfe144832154923b",
+    "url": "/TODO-React-App/index.html"
+  },
+  {
+    "revision": "9daa5ec41aec1f80185c",
+    "url": "/TODO-React-App/static/css/main.f9ac9abb.chunk.css"
+  },
+  {
+    "revision": "a288ab24967c04d3bcba",
+    "url": "/TODO-React-App/static/js/2.ffc71a18.chunk.js"
+  },
+  {
+    "revision": "9daa5ec41aec1f80185c",
+    "url": "/TODO-React-App/static/js/main.fdb67f09.chunk.js"
+  },
+  {
+    "revision": "ac39594ec5642b0879ea",
+    "url": "/TODO-React-App/static/js/runtime~main.a78e5d34.js"
+  }
+]);
