4e8499e7a44b8d558a128018be818b0361f1596a
ma1ex <matvienko.alexey@gmail.com>
2019-07-17 15:49:49 +0300

    Added an Input to add new Item.

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -4,13 +4,35 @@ import './item-add-form.css';
 
 export default class ItemAddForm extends Component {
 
+    state = {
+        label: ''
+    }
+
+    onLabelChange = (e) => {
+        this.setState({
+            // Т.к. стэйт не зависит от предыдущего значения, можно передать значение напрямую
+            label: e.target.value
+        });
+    };
+
+    onSubmit = (e) => {
+        // Отмена перезагрузки страницы
+        e.preventDefault();
+        this.props.onItemAdded(this.state.label);
+        // Очистка поля после сабмита
+        this.setState({
+            label: ''
+        });
+    };
+    
     //
     render() {
         return (
-            <div className="item-add-form">
-                <button className="btn btn-outline-secondary" 
-                    onClick={ () => this.props.onItemAdded('Hello World') }>Add Item</button>
-            </div>
+            <form className="item-add-form d-flex" onSubmit={this.onSubmit}>
+                <input type="text" className="form-control" value={this.state.label} 
+                    onChange={this.onLabelChange} placeholder="What needs to be done" />
+                <button className="btn btn-outline-secondary">Add Item</button>
+            </form>
         );
     }
 }
